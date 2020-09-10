310cfaaf0e257b18a9d09f06a7bca0c439bb1118
Swetabh suman <swtbhsmn@gmail.com>
2020-09-10 12:48:14 +0530

    Updates

---
+++ precache-manifest.e496d8dd2f58c2111ba56330072abb79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd6f9e76e02e641739d7e952eadc624",
+    "url": "/GithubCompare/index.html"
+  },
+  {
+    "revision": "6bd7e40d9ecb70d2721c",
+    "url": "/GithubCompare/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "26561e3d7eefd6ad0b7b",
+    "url": "/GithubCompare/static/css/main.5ed21f4c.chunk.css"
+  },
+  {
+    "revision": "6bd7e40d9ecb70d2721c",
+    "url": "/GithubCompare/static/js/2.9c6635b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GithubCompare/static/js/2.9c6635b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26561e3d7eefd6ad0b7b",
+    "url": "/GithubCompare/static/js/main.5167012a.chunk.js"
+  },
+  {
+    "revision": "cfe8f4f36017c936ee0c",
+    "url": "/GithubCompare/static/js/runtime-main.3863e2d2.js"
+  }
+]);
